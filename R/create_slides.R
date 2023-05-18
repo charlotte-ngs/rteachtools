e25a293ac018df38f1168d2888a454ff899fd64c
charlotte-ngs <peter.vonrohr@qualitasag.ch>
2023-05-18 06:32:00 +0200

    Corrected creation of new set of slides by correcting the path from where temporary template file is read

--- R/create_slides.R
+++ R/create_slides.R
@@ -38,26 +38,30 @@ create_slides <- function(ps_sl_path,
   if (tolower(fs::path_ext(s_sl_path)) != "rmd"){
     s_sl_path <- paste0(fs::path_ext_remove(s_sl_path), ".Rmd")
   }
+  # get filename
+  s_sl_file <- basename(s_sl_path)
   # check whether parent dir of s_sl_path exists
   s_sl_dir <- dirname(s_sl_path)
   if (!dir.exists(s_sl_dir)) dir.create(path = s_sl_dir, recursive = TRUE)
   # use rmarkdown draft to get to the template
-  s_sl_name <- fs::path_ext_remove(basename(s_sl_path))
+  s_sl_name <- fs::path_ext_remove(s_sl_file)
   # draft from template to tempdir
-  s_sl_tmp_path <- file.path(tempdir(), s_sl_name)
-  rmarkdown::draft(file = s_sl_tmp_path,
+  s_sl_tmp_dir <- file.path(tempdir(), s_sl_name)
+  rmarkdown::draft(file = s_sl_tmp_dir,
                    template = ps_template,
                    package = ps_package,
-                   edit = pb_edit)
+                   edit = FALSE)
   # read the template into a vector
-  vec_sl_tmpl <- readLines(s_sl_tmp_path)
+  vec_sl_tmpl <- readLines(con = file.path(s_sl_tmp_dir, s_sl_file))
   # delete template file, as it is not longer needed
-  fs::file_delete(path = s_sl_tmp_path)
+  fs::dir_delete(path = s_sl_tmp_dir)
   # replace placeholders with data in pl_data
   vec_sl_out <- whisker::whisker.render(template = vec_sl_tmpl,
                                         data = pl_data)
 
   # write content to file
   cat(paste0(vec_sl_out, collapse = "\n"), "\n", file = s_sl_path, append = FALSE)
+  # open newly created file
+  if (pb_edit) usethis::edit_file(path = s_sl_path)
   return(invisible(TRUE))
 }
